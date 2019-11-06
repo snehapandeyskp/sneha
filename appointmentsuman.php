340256cab4f71b15938e106238752f9832e57771
snehapandeyskp <50939796+snehapandeyskp@users.noreply.github.com>
2019-11-06 09:12:50 +0530

    Add files via upload
    i wanted to restrict the selection  of timing if its already selected once in a single date

---
+++ appointmentsuman.php
@@ -0,0 +1,368 @@
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>healthcare</title>
+    <link rel="icon" href="img/favicon.png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="css/flaticon.css">
+    <!-- magnific popup CSS -->
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <!-- nice select CSS -->
+    <link rel="stylesheet" href="css/nice-select.css">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="css/slick.css">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+	
+	
+	
+	
+ <style type="text/css">
+.form-style-7{
+	max-width:400px;
+	margin:50px auto;
+	background:;
+	border-radius:2px;
+	padding:20px;
+	font-family: Georgia, "Times New Roman", Times, serif;
+}
+.form-style-7 h1{
+	display: block;
+	text-align: center;
+	padding: 0;
+	margin: 0px 0px 20px 0px;
+	font-size:x-large;
+}
+.form-style-7 ul{
+	list-style:none;
+	padding:0;
+	margin:0;	
+}
+.form-style-7 li{
+	display: block;
+	padding: 9px;
+	border:1px solid  blue;
+	margin-bottom: 30px;
+	border-radius: 3px;
+}
+.form-style-7 li:last-child{
+	border:none;
+	margin-bottom: 0px;
+	text-align: center;
+}
+.form-style-7 li > label{
+	display: block;
+	float: left;
+	margin-top: -19px;
+	background:#FFFFFF ;
+	height: 14px;
+	padding: 0px  2px 2px 5px;
+	font-size: 12px;
+	overflow: hidden;
+	font-family: Arial, Helvetica, sans-serif;
+}
+.form-style-7 input[type="text"],
+.form-style-7 input[type="date"],
+.form-style-7 input[type="number"],
+.form-style-7 input[type="time"],
+
+.form-style-7 textarea,
+.form-style-7 select 
+{
+	box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	width: 100%;
+	display: block;
+	outline: none;
+	border: none;
+	height: 20px;
+	line-height: 25px;
+	font-size: 16px;
+	padding: 0;
+	font-family: Georgia, "Times New Roman", Times, serif;
+}
+.form-style-7 input[type="text"]:focus,
+.form-style-7 input[type="date"]:focus,
+.form-style-7 input[type="number"]:focus,
+.form-style-7 input[type="time"]:focus,
+.form-style-7 textarea:focus,
+.form-style-7 select:focus 
+{
+}
+.form-style-7 li > span{
+	display: block;
+	padding: 3px;
+	margin: 0 -9px -9px -9px;
+	text-align: center;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 11px;
+}
+.form-style-7 textarea{
+	resize:none;
+}
+.form-style-7 input[type="submit"],
+.form-style-7 input[type="button"]{
+	background: #2471FF;
+	border: none;
+	padding: 10px 20px 10px 20px;
+	border-bottom: 3px solid #5994FF;
+	border-radius: 3px;
+	color: #D2E2FF;
+}
+.form-style-7 input[type="submit"]:hover,
+.form-style-7 input[type="button"]:hover{
+	background:#F3F3F3 ;
+}
+</style>
+</head>
+
+<body>
+<div class="icon-bar">
+  <a href="#" class="facebook"><i class="fa fa-facebook"></i></a> 
+  <a href="#" class="twitter"><i class="fa fa-twitter"></i></a> 
+  <a href="#" class="google"><i class="fa fa-google"></i></a> 
+  <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+  <a href="#" class="youtube"><i class="fa fa-youtube"></i></a> 
+</div>
+    <!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="finalindex.php"><img src="logo11.png" alt="logo"  height="82" width="102"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse main-menu-item justify-content-center"
+                            id="navbarSupportedContent">
+                            <ul class="navbar-nav align-items-center">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="finalindex.php">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.php">about</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="Doctor.php"> doctors</a>
+                                </li>
+								<li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown"
+                                 role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                   OPD
+                                 </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                  <a class="dropdown-item href="reg3.php">Register here</a>
+                                <a class="dropdown-item" href="registered.php">Registered Patient</a>
+                                <a class="dropdown-item" href="vaccination.php">Vaccination chart</a>
+							
+                                </div>
+                                </li>
+                                <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
+                                role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                 DASHBOARD
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                                <a class="dropdown-item" href="sumanlogin.php">Admin</a>
+                                <a class="dropdown-item" href="single-blog.html">doctor </a>
+							    <a class="dropdown-item" href="blog.html">receptionist</a>
+							    </div>
+								 <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1"
+                                role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                 payment
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown_1">
+                                <a class="dropdown-item" href="viewbilling.php">offline billing</a>
+                                <a class="dropdown-item" href="onlinepayment.php">online payment</a>
+							   
+							    </div>
+                                </li>
+								<li class="nav-item">
+                                <a class="nav-link" href="contactus.php">Contact</a>
+                                </li>
+                                </ul>
+                                </div>
+                                  <a class="btn_2 d-none d-lg-block" href="#">HOT LINE- 7762884802</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
+
+<body>
+<script type="text/javascript">
+//auto expand textarea
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
+</script>
+<br>
+<br><br><br><br>
+<marquee><h3> <b>Book your appointment here<b> </h3> </marquee>
+<form class="form-style-7" action="appointmentsuman1.php" method="POST">
+<fieldset>
+<ul>
+<li>
+    <label for="name">Patient name*</label>
+    <input type="text" name="name" placeholder="Enter the name" maxlength="18" pattern="[a-zA-Z]{1,}" required />
+	<span style="color:blue">ONLY ALPHABETS WILL BE ACCEPTED</span>
+</li>
+
+<li>
+    <label for="mobile">Phone no*</label>
+    <input name="mobile" type="text"  placeholder="Contact number" maxlength="10"   pattern="[789][0-9]{9}" onclick="phonenumber()" required />
+	<span style="color:blue">PLEASE ENTER YOUR VALID PHONE NO </span>
+</li>
+<li>
+    <label for="Email">Email</label>
+    <input type="email" name="email" placeholder="example@gmail.com" maxlength="100" required>
+</li>
+<li>
+<label for="date">date </label>
+<input type="date"  placeholder="date" name="date" onchange="refreshtime(this.value);" min="<?php echo date('Y-m-d');?>" required>
+</li>
+
+<li>
+	<script>
+		function refreshtime(val){
+			//alert("refreshing"+val);
+			$( "#time" ).load("appointmentsuman.php?date=" + val + " #time" );
+		}
+		function check()
+        {
+
+       var pass1 = document.getElementById('mobile');
+
+
+       var message = document.getElementById('message');
+
+      var goodColor = "#0C6";
+      var badColor = "#FF9B37";
+
+      if(mobile.value.length!=10){
+
+        mobile.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "required 10 digits, match requested format!"
+      }
+	  function phonenumber(inputtext)
+	  {
+		  var phoneno=/^\d{10}$/;
+		  if(inputtxt.value.match(phoneno))
+		  {
+			  return true;
+		  }
+		  else
+		  { 
+	    alert ("");
+	    return false;
+		  }
+	  }
+	</script>
+	  <label>choose timing </label>
+	<div id="time">
+	<select name="time">
+	<?php
+		$arr[0]="10:00 AM";
+		$arr[1]="10:30 AM";
+		$arr[2]="11:00 AM";
+		$arr[3]="11:30 AM";
+		$arr[4]="12:00 PM";
+		$arr[5]="12:30 PM";
+		$arr[6]="06:00 PM";
+		$arr[7]="06:30 PM";
+		$arr[8]="07:00 PM";
+		$arr[9]="07:30 PM";
+		$arr[10]="08:00 PM";
+		$arr[11]="08:30 PM";
+	
+		$arrcheck[0]="";
+		$arrcheck[1]="";
+		$arrcheck[2]="";
+		$arrcheck[3]="";
+		$arrcheck[4]="";
+		$arrcheck[5]="";
+		$arrcheck[6]="";
+		$arrcheck[7]="";
+		$arrcheck[8]="";
+		$arrcheck[9]="";
+		$arrcheck[10]="";
+		$arrcheck[11]="";
+		
+		$i=0;
+		$adate=$_GET["date"];
+		$conn= mysqli_connect("localhost","root","","adress");
+		$sql = "SELECT * FROM clinicappointment WHERE date='$adate'";
+		$result=$conn->query($sql);
+		if ($result->num_rows > 0) {
+			while($row=$result->fetch_assoc()) {
+				$time=$row["time"];
+				$date=$row["date"];
+				echo "<option value=\"$time\">$time</option>";
+				$arrcheck[$i]=$time;
+				$i++;
+			}
+		}
+		
+		for($i=0;$i<=11;$i++){
+			$set="enabled";
+			for($j=0;$j<=11;$j++){
+				if($arr[$i]==$arrcheck[$j]){
+					$set="disabled";
+					break;
+				}
+			}
+			echo "<option value=\"$arr[$i]\" $set>$arr[$i]</option>";
+		}
+	
+	?>
+	<!---
+	<option></option>
+    <option value="10:00 AM">10.00 AM</option>
+    <option value="10:30 AM">10.30 AM</option>
+	<option value="11:00 AM" disabled>11.00 AM</option>
+    <option value="11:30 AM">11.30 AM</option>
+	<option value="12:00 AM">12.00 AM</option>
+    <option value="12:30 AM">12.30 AM</option>
+	<option value="06:00 PM">06.00 PM</option>
+    <option value="06:30 PM">06.30 PM</option>
+	<option value="07:00 PM">07.00 PM</option>
+    <option value="07:30 PM">07.30 PM</option>
+	<option value="08:00 PM">08.00 PM</option>
+    <option value="08:30 PM">08.30 PM</option>
+	<option value="text">other</option>
+	--->
+	</select>
+	</div>
+</li>
+
+<center>
+<input type="submit" name="submit">
+</center>
+</fieldset>
+</form>
+
+</body>
+</html>
+
